60a826ef5258cb413e7721cdd52b93e1e15edd4d
ricardocvel <ricardocampos_pr@live.com>
2020-11-17 20:25:50 -0300

    files generated when creating the project

---
+++ NUIXaml_Exemplos/NUIXaml_Exemplos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace NUIXaml_Exemplos
+{
+    public class Program : NUIApplication
+    {
+        Animation animation;
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Window window = Window.Instance;
+            window.BackgroundColor = Color.Blue;
+            window.KeyEvent += OnKeyEvent;
+
+            XamlPage page = new XamlPage();
+            page.PositionUsesPivotPoint = true;
+            page.ParentOrigin = ParentOrigin.Center;
+            page.PivotPoint = PivotPoint.Center;
+            page.Size = new Size(720, 1280, 0);
+            window.Add(page);
+
+            animation = new Animation(2000);
+            animation.AnimateTo(page.text, "Orientation", new Rotation(new Radian(new Degree(180.0f)), PositionAxis.X), 0, 500);
+            animation.AnimateTo(page.text, "Orientation", new Rotation(new Radian(new Degree(0.0f)), PositionAxis.X), 500, 1000);
+            animation.Looping = true;
+            animation.Play();
+        }
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
